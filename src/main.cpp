ae799818ddd32aa73a0c4c24bc20ff046a4cf844
Max <maxtvangils@gmail.com>
2024-06-12 10:57:05 +0200

    [FIXED] getMACAddressString function

--- src/main.cpp
+++ src/main.cpp
@@ -165,14 +165,12 @@ void connectToWifi() {
 }
 
 char* getMACAddressString() {
-//  byte mac[6];
-//  WiFi.macAddress(mac);
-//
-//  char macStr[18];
-//  sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-//
-//  return macStr;
-    return "test-mac";
+ byte mac[6];
+ WiFi.macAddress(mac);
 
+ static char macStr[18];
+ sprintf(macStr, "%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+ return macStr;
 }
 
